1134d3e560cfeaeb870fd0025a156398be51f7c2
unitax13 <theunitax@gmail.com>
2020-03-05 17:15:31 +0100

    hmm

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h> //usun potem
+#include <unistd.h>
 #include "generation.h" //usun potem
 #include "draw.h"
 
 int main() {
-	generation_t* gen = createNewGeneration(20, 10);
+	generation_t* gen = createNewGeneration(10, 10);
 	draw(gen);
-
+    sleep(1);
+    gen->cells[3][9] = ALIVE;
+    draw(gen);
 	return 0;
 }
